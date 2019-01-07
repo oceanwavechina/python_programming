e91514221fcd42c82c7eadc940b6b98909e8ab9d
liuyanan <oceanwave@yeah.net>
2019-01-07 17:16:01 +0800

    update

--- playground/magic_method.py
+++ playground/magic_method.py
@@ -20,18 +20,78 @@ class ConstructionAndInitialization(object):
     """
 
     def __new__(cls):
+        '''
+            控制如何实现生成一个新的实例，这是 类 级别的方法
+            1. 这个方法返回的是一个对象
+            2. 关于__new__的使用场景
+                2.1 重载 不可变类型 如 int， str tuple等
+        '''
         print_step(cls.__name__, sys._getframe().f_code.co_name)
-        return object.__new__(cls)
+        return super().__new__(cls)
 
     def __init__(self, filepath='./', filename='magic_method.py'):
+        '''
+            控制如何初始化一个已经生成好的实例，这是 对象 级别的方法
+        '''
         self.file = open(join(filepath, filename), 'r+')
         print_step(self.__class__.__name__, sys._getframe().f_code.co_name)
     
     def __del__(self):
+        '''
+            其实这个也不常用，因为并不能保证在它一定运行
+        '''
         self.file.close()
         del self.file
         print_step(self.__class__.__name__, sys._getframe().f_code.co_name)
 
 
+class EmployeeSalary(tuple):
+
+    '''
+        python3 不在支持 __cmp__ 方法
+    '''
+
+    def __new__(cls, iterable):
+        '''
+            这里算是__new__的一个场景，因为工资一定是整数，
+            我们要在创建对象时保证
+        '''
+        print(cls.__name__, " ", sys._getframe().f_code.co_name, ', with param:', iterable)
+        target_iterable = []
+        for item in iterable:
+            target_iterable.append(abs(int(item)))
+        return super().__new__(cls, target_iterable)
+
+    def __gt__(self, other):
+        '''
+            我们比较基本工资和奖金的和
+        '''
+        print(self.__class__.__name__, " ", sys._getframe().f_code.co_name, ', sum(self):', sum(self), ', sum(other):', sum(other))
+        return  sum(self) > sum(other)
+
+    def __eq__(self, other):
+        print(self.__class__.__name__, " ", sys._getframe().f_code.co_name, ', sum(self):', sum(self), ', sum(other):', sum(other))
+        return  sum(self) == sum(other)
+
+    def __lt__(self, other):
+        '''
+            sorted函数使用的是这个方法
+        '''
+        print(self.__class__.__name__, " ", sys._getframe().f_code.co_name, ', sum(self):', sum(self), ', sum(other):', sum(other))
+        return  sum(self) < sum(other)
+
+
 if __name__ == "__main__":
+    '''
+        del 执行的时机
+    '''
     a = ConstructionAndInitialization()
+
+    a = EmployeeSalary([-30, 100])
+    b = EmployeeSalary([20, 200])
+
+    print('salary > compare:', a > b)
+    print('salary == compare:', a == b)
+    L = [a, b]
+    print('sorted([a, b]): ')
+    print('\t ', sorted(L))
--- playground/tricks_2.py
+++ playground/tricks_2.py
@@ -14,3 +14,18 @@ class PropertyCache(object):
         value = self.func(obj)
         setattr(obj, self.func.__name__, value)
         return value
+
+
+class Foo:
+    def __init__(self):
+        self._property_to_be_cached = 'result'
+
+    @PropertyCache
+    def property_to_be_cached(self):
+        print('compute')
+        return self._property_to_be_cached
+
+test = Foo()
+
+print(test.property_to_be_cached)
+print(test.property_to_be_cached)
